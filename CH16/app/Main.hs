e438846881d0934ece1d34c1f8a0402157e216d1
Xiaofeng Ge <backofhan@gmail.com>
2017-10-25 20:00:08 +0800

    Finished 16.10

--- CH16/app/Main.hs
+++ CH16/app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import Lib
+import Test.QuickCheck
+import Test.QuickCheck.Function
 
 main :: IO ()
 main = do
@@ -9,3 +11,23 @@ main = do
   putStrLn "Use 'stack ghci' to start ghci to play around. Enjoy."
 
   -- Examples for playing around
+  -- quickCheck $ \x -> functorIdentity (x :: Identity Int)
+  -- quickCheck (functorCompose :: Identity Int -> Fun Int Int -> Fun Int Int -> Bool)
+
+  -- quickCheck $ \x -> functorIdentity (x :: Pair Char)
+  -- quickCheck (functorCompose :: Pair Int -> Fun Int Char -> Fun Char String -> Bool)
+
+  -- quickCheck $ \x -> functorIdentity (x :: Two Char Int)
+  -- quickCheck (functorCompose :: Two Int Char -> Fun Char Int -> Fun Int String -> Bool)
+
+  -- quickCheck $ \x -> functorIdentity (x :: Three Char Int String)
+  -- quickCheck (functorCompose :: Three Int Char String -> Fun String Int -> Fun Int String -> Bool)
+
+  -- quickCheck $ \x -> functorIdentity (x :: Three' Char Int)
+  -- quickCheck (functorCompose :: Three' Int String -> Fun String String -> Fun String Char -> Bool)
+
+  -- quickCheck $ \x -> functorIdentity (x :: Four Char Int Char String)
+  -- quickCheck (functorCompose :: Four Int String Char Int -> Fun Int String -> Fun String Char -> Bool)
+
+  -- quickCheck $ \x -> functorIdentity (x :: Four' Char Int)
+  -- quickCheck (functorCompose :: Four' Char Int -> Fun Int String -> Fun String Char -> Bool)
--- CH16/src/Lib.hs
+++ CH16/src/Lib.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE InstanceSigs #-}
 module Lib where
 
+import Test.QuickCheck
+import Test.QuickCheck.Function
 
 -- 16.4 Exercises: Be Kind
   -- 1 a: *
@@ -33,3 +35,78 @@ d' = fmap ((return '1' ++). show) (\x -> [x, 1..3])
 e = let ioi = readIO "1" :: IO Integer
         changed = fmap (read.("123" ++).show) ioi
     in fmap (*3) changed
+
+-- 16.10 Exercises: Instances of Func
+functorIdentity :: (Functor f, Eq (f a)) => f a -> Bool
+functorIdentity f = fmap id f == f
+
+functorCompose :: (Eq (f c), Functor f) => f a -> Fun a b -> Fun b c -> Bool
+functorCompose x (Fun _ f) (Fun _ g) =
+  (fmap (g.f) x) == (fmap g . fmap f $ x)
+
+  -- 1
+newtype Identity a = Identity a deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+
+instance (Arbitrary a) => Arbitrary (Identity a) where
+  arbitrary = Identity <$> arbitrary
+
+  -- 2
+data Pair a = Pair a a deriving (Eq, Show)
+
+instance Functor Pair where
+  fmap f (Pair a1 a2) = Pair (f a1) (f a2)
+
+instance (Arbitrary a) => Arbitrary (Pair a) where
+  arbitrary = Pair <$> arbitrary <*> arbitrary
+
+  -- 3
+data Two a b = Two a b deriving (Eq, Show)
+
+instance Functor (Two a) where
+  fmap f (Two a b) = Two a (f b)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = Two <$> arbitrary <*> arbitrary
+
+  -- 4
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance Functor (Three a b) where
+  fmap f (Three a b c) = Three a b (f c)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = Three <$> arbitrary <*> arbitrary <*> arbitrary
+
+  -- 5
+data Three' a b = Three' a b b deriving (Eq, Show)
+
+instance Functor (Three' a) where
+  fmap f (Three' a b1 b2) = Three' a (f b1) (f b2)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Three' a b) where
+  arbitrary = Three' <$> arbitrary <*> arbitrary <*> arbitrary
+
+  -- 6
+data Four a b c d = Four a b c d deriving (Eq, Show)
+
+instance Functor (Four a b c) where
+  fmap f (Four a b c d) = Four a b c (f d)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) => Arbitrary (Four a b c d) where
+  arbitrary = Four <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+  -- 7
+data Four' a b = Four' a a a b deriving (Eq, Show)
+
+instance Functor (Four' a) where
+  fmap f (Four' a1 a2 a3 b) = Four' a1 a2 a3 (f b)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Four' a b) where
+  arbitrary = Four' <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+  -- 8
+-- Can not implement because Trival has kind *
+-- Only possible to implement functor for type which has kind * -> *
