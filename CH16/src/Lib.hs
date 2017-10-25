aa7b998726ca272c307e2f82ee57b83d4131a006
Xiaofeng Ge <backofhan@gmail.com>
2017-10-25 16:30:38 +0800

    Finish 16.7

--- CH16/src/Lib.hs
+++ CH16/src/Lib.hs
@@ -2,8 +2,34 @@
 module Lib where
 
 
--- 2.4 Exercises: Be Kind
+-- 16.4 Exercises: Be Kind
   -- 1 a: *
   -- 2 b: * -> *   T: * -> *
   -- 3 c: * -> * -> *
-   
+
+-- 16.7 Exercises: Heavy Lifting
+  -- 1
+-- a = (+1) $ read "[1]" :: [Int]
+a = fmap (+1) $ read "[1]" :: [Int]
+
+  -- 2
+-- b = (++ "lol") (Just ["Hi,", "Hello"])
+b = (fmap.fmap) (++ "lol") (Just ["Hi,", "Hello"])
+
+  -- 3
+-- c = (*2) (\x -> x - 2)
+c = fmap (*2) (\x -> x - 2)
+c' = (*2).(\x -> x - 2)
+
+  -- 4
+-- d = ((return '1' ++). show) (\x -> [x, 1..3])
+d = ((return '1' ++). show).(\x -> [x, 1..3])
+d' = fmap ((return '1' ++). show) (\x -> [x, 1..3])
+
+  -- 5
+-- e = let ioi = readIO "1" :: IO Integer
+--         changed = read ("123" ++) show ioi
+--     in (*3) changed
+e = let ioi = readIO "1" :: IO Integer
+        changed = fmap (read.("123" ++).show) ioi
+    in fmap (*3) changed
