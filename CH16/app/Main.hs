399d927ae61f33238916e052aa87e5123937d57d
Xiaofeng Ge <backofhan@gmail.com>
2017-10-25 11:15:11 +0800

    Added CH16

---
+++ CH16/app/Main.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import Lib
+
+main :: IO ()
+main = do
+  putStrLn "Excercises from Chapter 16: Functor."
+  putStrLn "Some examples commented out in main function."
+  putStrLn "Use 'stack ghci' to start ghci to play around. Enjoy."
+
+  -- Examples for playing around
---
+++ CH16/src/Lib.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE InstanceSigs #-}
+module Lib where
+
+
+-- 2.4 Exercises: Be Kind
+  -- 1 a: *
+  -- 2 b: * -> *   T: * -> *
+  -- 3 c: * -> * -> *
+   
