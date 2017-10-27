740073b1d721a4d8d974f4dbd8cb812463dd39a7
Xiaofeng Ge <backofhan@gmail.com>
2017-10-27 22:37:43 +0800

    In the middle

--- CH15/app/Main.hs
+++ CH15/app/Main.hs
@@ -2,6 +2,8 @@ module Main where
 
 import Lib
 import Test.QuickCheck
+import Data.Monoid hiding((<>))
+import Data.Semigroup
 
 main :: IO ()
 main = do
@@ -12,3 +14,7 @@ main = do
   -- quickCheck (monoidAssoc :: First' String -> First' String -> First' String -> Bool)
   -- quickCheck (monoidLeftIdentity :: First' String -> Bool)
   -- quickCheck (monoidRightIdentity :: First' String -> Bool)
+
+  -- quickCheck (semigroupAssoc :: Trivial -> Trivial -> Trivial -> Bool)
+  -- quickCheck (semigroupAssoc :: Identity String -> Identity String -> Identity String -> Bool)
+  -- quickCheck (semigroupAssoc :: Two String (Sum Int) -> Two String (Sum Int) -> Two String (Sum Int) -> Bool)
--- CH15/src/Lib.hs
+++ CH15/src/Lib.hs
@@ -2,7 +2,8 @@
 module Lib where
 
 import Test.QuickCheck
-import Data.Monoid
+import Data.Monoid hiding((<>))
+import Data.Semigroup
 
 -- 15.10 Exercise: Optional Monoid
 data Optional a =
@@ -31,10 +32,60 @@ instance Arbitrary a => Arbitrary (First' a) where
   arbitrary = First' <$> arbitrary
 
 monoidAssoc :: (Eq m, Monoid m) => m -> m -> m -> Bool
-monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+monoidAssoc a b c = (a `mappend` (b `mappend` c)) == ((a `mappend` b) `mappend` c)
 
 monoidLeftIdentity :: (Eq m, Monoid m) => m -> Bool
-monoidLeftIdentity a = (mempty <> a ) == a
+monoidLeftIdentity a = (mempty `mappend` a ) == a
 
 monoidRightIdentity :: (Eq m, Monoid m) => m -> Bool
-monoidRightIdentity a = (a <> mempty ) == a
+monoidRightIdentity a = (a `mappend` mempty ) == a
+
+-- 15.14 Chapter Exercises
+  -- Smigroup exercises
+    -- 1
+data Trivial = Trivial deriving (Eq, Show)
+
+instance Semigroup Trivial where
+  _ <> _ = Trivial
+
+instance Arbitrary Trivial where
+  arbitrary = return Trivial
+
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+    -- 2
+newtype Identity a = Identity a deriving (Eq, Show)
+
+instance Semigroup a => Semigroup (Identity a) where
+  (Identity a) <> (Identity b) = Identity (a <> b)
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = Identity <$> arbitrary
+
+    -- 3
+data Two a b = Two a b deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
+  (Two a1 b1) <> (Two a2 b2) = Two (a1 <> a2) (b1 <> b2)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = Two <$> arbitrary <*> arbitrary
+
+    -- 4
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b, Semigroup c) => Semigroup (Three a b c) where
+  (Three a1 b1 c1) <> (Three a2 b2 c2) = Three (a1 <> a2) (b1 <> b2) (c1 <> c2)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = Three <$> arbitrary <*> arbitrary <*> arbitrary
+
+    -- 5
+data Four a b c d = Four a b c d deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b, Semigroup c, Semigroup d) => Semigroup (Four a b c d) where
+  (Four a1 b1 c1 d1) <> (Four a2 b2 c2 d2) = Four (a1 <> a2) (b1 <> b2) (c1 <> c2) (d1 <> d2)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) => Arbitrary (Four a b c d) where
+  arbitrary = Four <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
