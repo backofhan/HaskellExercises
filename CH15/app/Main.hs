2a19ddcf723513916c5f8f921def98b12caac8f6
Xiaofeng Ge <backofhan@gmail.com>
2017-10-27 15:47:57 +0800

    Finished 15.12

--- CH15/app/Main.hs
+++ CH15/app/Main.hs
@@ -1,9 +1,14 @@
 module Main where
 
 import Lib
+import Test.QuickCheck
 
 main :: IO ()
 main = do
   putStrLn "Excercises from Chapter 15: Monoid, Semigroup."
   putStrLn "Some examples commented out in main function."
   putStrLn "Use 'stack ghci' to start ghci to play around. Enjoy."
+
+  -- quickCheck (monoidAssoc :: First' String -> First' String -> First' String -> Bool)
+  -- quickCheck (monoidLeftIdentity :: First' String -> Bool)
+  -- quickCheck (monoidRightIdentity :: First' String -> Bool)
--- CH15/src/Lib.hs
+++ CH15/src/Lib.hs
@@ -1,1 +1,40 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Lib where
+
+import Test.QuickCheck
+import Data.Monoid
+
+-- 15.10 Exercise: Optional Monoid
+data Optional a =
+    Nada
+  | Only a
+  deriving (Eq, Show)
+
+instance Monoid a => Monoid (Optional a) where
+  mempty = Nada
+  mappend (Only a) (Only b) = Only $ a `mappend` b
+  mappend Nada a = a
+  mappend a Nada = a
+
+-- 15.12 Exercise: Maybe Another Monoid
+newtype First' a = First' { getFirst' :: Optional a } deriving (Eq, Show)
+
+instance Monoid (First' a) where
+  mempty = First' Nada
+  mappend l@(First' (Only a)) _ = l
+  mappend (First' Nada) r = r
+
+instance Arbitrary a => Arbitrary (Optional a) where
+  arbitrary = frequency [(1, return Nada), (3, Only <$> arbitrary)]
+
+instance Arbitrary a => Arbitrary (First' a) where
+  arbitrary = First' <$> arbitrary
+
+monoidAssoc :: (Eq m, Monoid m) => m -> m -> m -> Bool
+monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+monoidLeftIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidLeftIdentity a = (mempty <> a ) == a
+
+monoidRightIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidRightIdentity a = (a <> mempty ) == a
