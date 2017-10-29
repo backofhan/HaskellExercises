868b28fd60acca3de508daaf0afb1da01fb7bdf4
Xiaofeng Ge <backofhan@gmail.com>
2017-10-29 22:42:52 +0800

    Missing

--- CH17/app/Main.hs
+++ CH17/app/Main.hs
@@ -8,3 +8,9 @@ main = do
   putStrLn "Excercises from Chapter 17: Applicative."
   putStrLn "Some examples commented out in main function."
   putStrLn "Use 'stack ghci' to start ghci to play around. Enjoy."
+
+  -- Exercise: Fixer Upper
+    -- 1
+  -- const <$> Just "Hello" <*> pure "World"
+    -- 2
+  -- (,,,) <$> Just 90 <*> Just 10 <*> Just "Tierness" <*> pure [1,2,3]
