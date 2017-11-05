7a6af46cf47bfc62b1c6f01e23abf3cd0a9e8292
Xiaofeng Ge <backofhan@gmail.com>
2017-11-05 18:39:12 +0800

    Finish CH20

--- CH20/src/Lib.hs
+++ CH20/src/Lib.hs
@@ -67,3 +67,39 @@ fold' = foldMap id
     --10
 foldMap' :: (Foldable t, Monoid m) => (a -> m) -> t a -> m
 foldMap' f = foldr (\x b -> f x <> b) mempty
+
+-- 20.6 Chapter Exercises
+  --1
+data Constant a b = Constant a deriving (Show)
+
+instance Foldable (Constant a) where
+  foldr _ b _ = b
+  --foldMap _ _ = mempty -- Also works
+
+  --2
+data Two a b = Two a b deriving (Show)
+
+instance Foldable (Two a) where
+  foldMap f (Two a b) = f b
+
+  --3
+data Three a b c = Three a b c
+
+instance Foldable (Three a b) where
+  foldMap f (Three a b c) = f c
+
+  --4
+data Three' a b = Three' a b b
+
+instance Foldable (Three' a) where
+  foldMap f (Three' a b1 b2) = f b1 <> f b2
+
+  --5
+data Four' a b = Four' a b b b
+
+instance Foldable (Four' a) where
+  foldMap f (Four' a b1 b2 b3) = f b1 <> f b2 <> f b3
+
+  -- Thinking cap time
+filterF :: (Applicative f, Foldable t, Monoid (f a)) => (a -> Bool) -> t a -> f a
+filterF f = foldMap (\a -> if f a then pure a else mempty)
